89e6a030a82ee205e730875b222429ca8d3fb95c
v <>
2020-09-27 10:10:12 -0400

    decrease debounce delay

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ const text = document.getElementById("type");
      text.addEventListener("keypress",send);
 
      function send(){
-       setTimeout(function(){worker.postMessage(text.value)}, 3000)
+       setTimeout(function(){worker.postMessage(text.value)}, 1000)
      }
 
      worker.onmessage = function (e) {
